7dcfa1f0a6e916e62312814e66a021129727219b
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-22 19:47:55 -0400

    added urls_new.ejs

--- express_server.js
+++ express_server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port
+function generateRandomString() {
+  
+}
 
+
+// sets app to use ejs as its view engine
 app.set("view engine", "ejs");
-
+// mock data base to hold urls
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.use(express.urlencoded({ extended: true }));
+
+//gets for various paths
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -18,12 +26,24 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// seriously dont forget about [] notation it helps to break down stuff in understandable variables
 app.get("/urls/:id", (req, res) => {
   const shortUrl = req.params.id;
   const longUrl = urlDatabase[shortUrl];
   const templateVars = { shortUrl, longUrl };
   res.render("urls_show", templateVars);
 });
+
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase)
